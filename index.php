f5cdbcd0bff5daf7c0211f6fd29158195fcfeb4e
unknown <Ananay Arora>
2015-07-26 10:48:28 +0530

    Added Login

--- index.php
+++ index.php
@@ -4,10 +4,11 @@
 	<title>Crowdfound</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<script type="text/javascript" src="https://code.jquery.com/jquery-latest.js"></script>
+	<script type="text/javascript" src="https://cdn.firebase.com/js/client/2.2.4/firebase.js"></script>
 	<script type="text/javascript" src="script.js"></script>
 </head>
 <body>
-	<span class="login">Login</span>
+	<span class="login"><span href="#" id="login">Login</span></span>
 	<div class="panel1">
 		<h1 class="title">Crowdfound</h1>
 	</div>
--- script.js
+++ script.js
@@ -4,6 +4,18 @@ $(document).ready(function(){
 			'marginTop':50,
 			'opacity':1
 		});
+		var srv = new Firebase("http://quizfork-server.firebaseio.com");
+		var authData = srv.getAuth();
+		if (authData){
+			var userData = authData.twitter.cachedUserProfile;
+			var pic = userData.profile_image_url_https;
+			$("#login").html('<img width="100" height="100" src="'+pic+'" class="user_pic" align="absmiddle"></img><span class="user_name">'+userData.name+'<br /></span>&nbsp;&nbsp;&nbsp;&nbsp;<a href="" id="logout">Logout</a>');
+		}
+		$(document).on('click','#logout', function(){
+			srv.unauth();
+			$(this).fadeOut();
+			authData = '';
+		});
 		$(window).scroll(function(){
 			if ($(window).scrollTop() >= $(window).height()){
 				$(".header").fadeIn();
@@ -11,5 +23,20 @@ $(document).ready(function(){
 				$(".header").fadeOut();
 			}
 		});
+		$("#login").click(function(){
+			if (authData){
+				window.location.href="parse-js-blank/index.html";
+			} else {
+				srv.authWithOAuthPopup("twitter", function(error, data){
+					if (error){
+						alert("Error Signing In");
+					} else {
+						var userData = data.twitter.cachedUserProfile;
+						var pic = userData.profile_image_url_https;
+						$("#login").html('<img width="100" height="100" src="'+pic+'" class="user_pic" align="absmiddle"></img><span class="user_name">'+userData.name+'<br /></span>&nbsp;&nbsp;&nbsp;&nbsp;<a href="" id="logout">Logout</a>');
+					}
+				});
+			}
+		});	
 	});
 });
